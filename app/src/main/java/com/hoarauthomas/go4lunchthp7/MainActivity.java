6c401bef97814f735dafe1f992cd3e4111b33053
HOARAU Thomas <perso.thomas.hoarau@gmail.com>
2021-06-04 13:03:17 +0200

    integration of Firebase-ui and test with google is success

--- app/src/main/java/com/hoarauthomas/go4lunchthp7/MainActivity.java
+++ app/src/main/java/com/hoarauthomas/go4lunchthp7/MainActivity.java
@@ -1,10 +1,14 @@
 package com.hoarauthomas.go4lunchthp7;
 
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 
 import com.firebase.ui.auth.AuthUI;
+import com.firebase.ui.auth.IdpResponse;
 
 import java.util.Arrays;
 import java.util.List;
@@ -13,7 +17,9 @@ public class MainActivity extends AppCompatActivity {
 
     private static final int RC_SIGN_IN = 123;
 
-    List<AuthUI.IdpConfig> providers = Arrays.asList(new AuthUI.IdpConfig.EmailBuilder().build());
+    List<AuthUI.IdpConfig> providers = Arrays.asList(
+            new AuthUI.IdpConfig.EmailBuilder().build(),
+            new AuthUI.IdpConfig.GoogleBuilder().build());
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -21,12 +27,13 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
 
+        //call Firebase Sign In Activity
         startSignInActivity();
 
 
     }
 
-
+    //open Firebase UI SignIn Activity
     private void startSignInActivity() {
         startActivityForResult(
                 AuthUI.getInstance()
@@ -34,5 +41,27 @@ public class MainActivity extends AppCompatActivity {
                         .setAvailableProviders(providers)
                         .build(), RC_SIGN_IN
         );
+    }
+
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        Log.i("THOMAS","Analyse retour authentificationau...");
+
+        IdpResponse response = IdpResponse.fromResultIntent(data);
+
+        if (requestCode == RC_SIGN_IN){
+
+            //success
+            if (resultCode == RESULT_OK){
+                Log.i("THOMAS","authentification ok");
+            }else {//error
+                if (response == null) {
+                    Log.i("THOMAS", "authentification nok");
+                }
+            }
+        }
     }
 }
