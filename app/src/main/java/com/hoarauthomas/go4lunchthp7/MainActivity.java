cb7fd0bdfdbf29aeb0ba9ed27369fdd29b019d70
HOARAU Thomas <perso.thomas.hoarau@gmail.com>
2021-06-04 12:13:41 +0200

    integration of Firebase-ui and test with mail is success

--- app/src/main/java/com/hoarauthomas/go4lunchthp7/MainActivity.java
+++ app/src/main/java/com/hoarauthomas/go4lunchthp7/MainActivity.java
@@ -4,11 +4,35 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
+import com.firebase.ui.auth.AuthUI;
+
+import java.util.Arrays;
+import java.util.List;
+
 public class MainActivity extends AppCompatActivity {
+
+    private static final int RC_SIGN_IN = 123;
+
+    List<AuthUI.IdpConfig> providers = Arrays.asList(new AuthUI.IdpConfig.EmailBuilder().build());
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+        startSignInActivity();
+
+
+    }
+
+
+    private void startSignInActivity() {
+        startActivityForResult(
+                AuthUI.getInstance()
+                        .createSignInIntentBuilder()
+                        .setAvailableProviders(providers)
+                        .build(), RC_SIGN_IN
+        );
     }
 }
