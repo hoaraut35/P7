431fe4f8ee5cd8b72259200f5e4ac38b37d3a9d3
HOARAU Thomas <perso.thomas.hoarau@gmail.com>
2021-06-04 14:50:44 +0200

    integration of view binding

--- app/src/main/java/com/hoarauthomas/go4lunchthp7/MainActivity.java
+++ app/src/main/java/com/hoarauthomas/go4lunchthp7/MainActivity.java
@@ -6,26 +6,52 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
 
 import com.firebase.ui.auth.AuthUI;
 import com.firebase.ui.auth.IdpResponse;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.hoarauthomas.go4lunchthp7.databinding.ActivityMainBinding;
 
 import java.util.Arrays;
 import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
 
+    //Added for view binding
+    private ActivityMainBinding binding;
+
     private static final int RC_SIGN_IN = 123;
+    private static final int SIGN_OUT_TASK = 10;
+    private static final int DELETE_USER_TASK = 20;
 
     List<AuthUI.IdpConfig> providers = Arrays.asList(
             new AuthUI.IdpConfig.EmailBuilder().build(),
             new AuthUI.IdpConfig.GoogleBuilder().build(),
             new AuthUI.IdpConfig.FacebookBuilder().build());
 
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        this.updateUIWhenResuming();
+
+    }
+
+    private void updateUIWhenResuming() {
+
+    }
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+
+        //setContentView(R.layout.activity_main);
+
+        binding = ActivityMainBinding.inflate(getLayoutInflater());
+        View view = binding.getRoot();
 
 
         //call Firebase Sign In Activity
@@ -49,20 +75,31 @@ public class MainActivity extends AppCompatActivity {
     protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
         super.onActivityResult(requestCode, resultCode, data);
 
-        Log.i("THOMAS","Analyse retour authentificationau...");
+        Log.i("THOMAS", "Analyse retour authentificationau...");
 
         IdpResponse response = IdpResponse.fromResultIntent(data);
 
-        if (requestCode == RC_SIGN_IN){
+        if (requestCode == RC_SIGN_IN) {
 
             //success
-            if (resultCode == RESULT_OK){
-                Log.i("THOMAS","authentification ok");
-            }else {//error
+            if (resultCode == RESULT_OK) {
+                Log.i("THOMAS", "authentification ok");
+            } else {//error
                 if (response == null) {
                     Log.i("THOMAS", "authentification nok");
                 }
             }
         }
     }
+
+    //get a singleton for current user
+    protected FirebaseUser getCurrentUser() {
+        return FirebaseAuth.getInstance().getCurrentUser();
+    }
+
+    //check if currentuser is connected or not
+    protected Boolean isCurrentUserLogged() {
+        return this.getCurrentUser() != null;
+    }
+
 }
