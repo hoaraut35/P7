268e1a5e5ab87059545e64b041edafe95396658a
HOARAU Thomas <perso.thomas.hoarau@gmail.com>
2021-06-08 11:22:12 +0200

    add theme for login and get error login

--- app/src/main/java/com/hoarauthomas/go4lunchthp7/MainActivity.java
+++ app/src/main/java/com/hoarauthomas/go4lunchthp7/MainActivity.java
@@ -9,6 +9,7 @@ import android.util.Log;
 import android.view.View;
 
 import com.firebase.ui.auth.AuthUI;
+import com.firebase.ui.auth.ErrorCodes;
 import com.firebase.ui.auth.IdpResponse;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
@@ -22,10 +23,12 @@ public class MainActivity extends AppCompatActivity {
     //Added for view binding
     private ActivityMainBinding binding;
 
+    //Added for return state
     private static final int RC_SIGN_IN = 123;
     private static final int SIGN_OUT_TASK = 10;
     private static final int DELETE_USER_TASK = 20;
 
+    //Added for login mode
     List<AuthUI.IdpConfig> providers = Arrays.asList(
             new AuthUI.IdpConfig.EmailBuilder().build(),
             new AuthUI.IdpConfig.GoogleBuilder().build(),
@@ -49,10 +52,8 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
 
         //setContentView(R.layout.activity_main);
-
         binding = ActivityMainBinding.inflate(getLayoutInflater());
         View view = binding.getRoot();
-
 
         //call Firebase Sign In Activity
         startSignInActivity();
@@ -61,21 +62,25 @@ public class MainActivity extends AppCompatActivity {
     }
 
     //open Firebase UI SignIn Activity
+    //TODO: add a theme for login form, create lmogo
     private void startSignInActivity() {
         startActivityForResult(
                 AuthUI.getInstance()
                         .createSignInIntentBuilder()
+                        .setTheme(R.style.LoginTheme)
                         .setAvailableProviders(providers)
+                        //.setLogo(R.drawable.ic_logo)
                         .build(), RC_SIGN_IN
         );
     }
 
 
+    //get the result after login fail or not
     @Override
     protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
         super.onActivityResult(requestCode, resultCode, data);
 
-        Log.i("THOMAS", "Analyse retour authentificationau...");
+        Log.i("THOMAS", "Retour authentification ...");
 
         IdpResponse response = IdpResponse.fromResultIntent(data);
 
@@ -83,12 +88,21 @@ public class MainActivity extends AppCompatActivity {
 
             //success
             if (resultCode == RESULT_OK) {
-                Log.i("THOMAS", "authentification ok");
+                Log.i("THOMAS", "authentification réussi");
             } else {//error
+
                 if (response == null) {
-                    Log.i("THOMAS", "authentification nok");
+                    Log.i("THOMAS", "authentification annulée");
+                } else if (response.getError().getErrorCode() == ErrorCodes.NO_NETWORK) {
+                    Log.i("THOMAS", "authentification impossible sans réseau");
+
+                } else if (response.getError().getErrorCode() == ErrorCodes.UNKNOWN_ERROR) {
+                    Log.i("THOMAS", "authentification impossible erreur inconnu");
                 }
+
+
             }
+
         }
     }
 
