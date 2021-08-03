b90943d0e45caeda43417556b779b232e7e8e470
HOARAU Thomas <perso.thomas.hoarau@gmail.com>
2021-08-03 18:56:26 +0200

    update distance in list of restaurant but must be moved to viewmodel after

--- app/src/main/java/com/hoarauthomas/go4lunchthp7/api/GooglePlaceApi.java
+++ app/src/main/java/com/hoarauthomas/go4lunchthp7/api/GooglePlaceApi.java
@@ -4,6 +4,7 @@ package com.hoarauthomas.go4lunchthp7.api;
 import com.hoarauthomas.go4lunchthp7.Result2;
 import com.hoarauthomas.go4lunchthp7.model.pojo.Place;
 
+import io.reactivex.Observable;
 import retrofit2.Call;
 import retrofit2.http.GET;
 import retrofit2.http.Query;
@@ -12,7 +13,8 @@ import retrofit2.http.Query;
 
 public interface GooglePlaceApi {
 
-    //for Place Search...
+    //without rxjava
+    // for Place Search...
     @GET("place/nearbysearch/json?rankby=distance&type=restaurant")
     Call<Place> getNearbyPlaces(
             @Query("key") String key,
@@ -24,11 +26,19 @@ public interface GooglePlaceApi {
             @Query("key") String key,
             @Query("place_id") String myPlaceId);
 
+    //**********************************************************************************************
 
+    //with rxjava
+    // for Place Search...
+    @GET("place/nearbysearch/json?rankby=distance&type=restaurant")
+    Observable<Place> getNearbyPlacesRx(
+            @Query("key") String key,
+            @Query("location") String myLocation);
 
-
-
-
-
+    //for Place Details...
+    @GET("place/details/json?fields=formatted_phone_number,url,rating,opening_hours")
+    Observable<Result2> getPlaceDetailsRx(
+            @Query("key") String key,
+            @Query("place_id") String myPlaceId);
 
 }
--- app/src/main/java/com/hoarauthomas/go4lunchthp7/api/RetrofitRequest.java
+++ app/src/main/java/com/hoarauthomas/go4lunchthp7/api/RetrofitRequest.java
@@ -1,12 +1,13 @@
 package com.hoarauthomas.go4lunchthp7.api;
 
 import retrofit2.Retrofit;
+import retrofit2.adapter.rxjava.RxJavaCallAdapterFactory;
 import retrofit2.converter.gson.GsonConverterFactory;
 
 public class RetrofitRequest {
 
+    //without rxjava
     private static Retrofit retrofit;
-
     public static Retrofit getRetrofitInstance() {
         if (retrofit == null) {
             retrofit = new Retrofit.Builder()
@@ -16,4 +17,23 @@ public class RetrofitRequest {
         }
         return retrofit;
     }
+
+    //**********************************************************************************************
+
+    //with rxjava for test
+    private static Retrofit retrofitRx;
+    public static Retrofit getRetrofitInstanceRx() {
+        if (retrofitRx == null) {
+            retrofitRx = new Retrofit.Builder()
+                    .baseUrl("https://maps.googleapis.com/maps/api/")
+                    .addConverterFactory(GsonConverterFactory.create())
+                    //Added for Rxjava
+                    .addCallAdapterFactory(RxJavaCallAdapterFactory.create())
+                    .build();
+        }
+        return retrofitRx;
+    }
+
+
+
 }
